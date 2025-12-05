a23ef495e88d32a0b86f5d7fba9ff7b152aa791b
Shardul-Pathak <shardulpathak796@gmail.com>
2025-12-06 00:41:48 +0530

    DB connection added

--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ dotenv.config()
 
 import express from "express"
 import cors from "cors"
+import { connect } from "mongoose"
 
 const app = express()
 const PORT = process.env.PORT || 3000
+connect(process.env.MONGODB_URI || "mongodb://localhost:27017/ElderEase")
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.error("Connection error:", err))
 
 app.use(cors())
 app.use(express.json())
