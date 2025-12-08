68e455ab730a58a820f38908a204ebdc33f1cd1d
Swanandi Kayarkar <rkswanandi@gmail.com>
2025-12-08 21:00:35 +0530

    Reminder Page, emergency page and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,14 @@ import Dashboard from "./components/dashboard";
 import Navbar from "./components/Navbar";
 import ReminderCard from "./components/ReminderCard";
 import ReminderForm from "./components/ReminderForm";
-
+import Reminders from "./pages/Reminders";
+import ContactCard from "./components/ContactCard";
+import ContactForm from "./components/ContactForm";
+import EmergencyContacts from "./pages/Emergency";
 export default function App() {
   return (
     <>
-      <ReminderCard />
+      <EmergencyContacts />
       
     </>
   );
