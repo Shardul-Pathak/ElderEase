095750b7657339d8b04774d1381c216b0b1a41ba
Shardul Pathak <shardulpathak796@gmail.com>
2025-12-12 23:29:15 +0530

    Update CORS origin to specific URL

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ import reminderRoutes from './routes/reminderRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors({
-    origin: "",
+    origin: "https://elder-ease-1dyk.vercel.app",
     credentials: true
 }));
 app.use(express.json());
